b318ea54d6f6148dc07b0fc9697c43786520eb13
guarise <andrea.guarise@to.infn.it>
2016-04-27 14:25:40 +0200

    Refactoring, moved test code within module.

--- connectors/Config.py
+++ connectors/Config.py
@@ -17,3 +17,17 @@ class Config:
     
     def has_option(self,section,key):
         return self.config.has_option(section,key)
+    
+
+#Test code
+if __name__ == '__main__':
+    conf = Config('./test.conf')
+    conf.read()
+    print "Get a value:"
+    print conf.get("Pillar","baseUri")
+    print "Get an array of item tuples:"
+    print conf.items("Metrics")
+    print "Test an existent item (True):"
+    print conf.has_option("Pillar","baseUri")
+    print "Test a non existent item (False):"
+    print conf.has_option("Not exists","baseUri")
--- connectors/test_config.py
+++
@@ -1,7 +0,0 @@
-import Config
-
-
-conf = Config.Config('./test.conf')
-conf.read()
-print conf.get("Pillar","baseUri")
-print conf.items("Metrics")
