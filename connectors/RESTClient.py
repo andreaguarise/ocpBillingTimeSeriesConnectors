8b27def6292ab56c0e6a4cf60407812646e7b764
guarise <andrea.guarise@to.infn.it>
2016-04-28 13:45:57 +0200

    Refactoring, moved test code

--- connectors/RESTClient.py
+++ connectors/RESTClient.py
@@ -1,5 +1,4 @@
 import json
-
 import requests 
 
 
@@ -15,4 +14,16 @@ class RESTClient:
     
     def json_body_load(self):
         return json.loads(self.body)
-    
+
+
+#test code    
+if __name__ == '__main__':    
+    rest = RESTClient('http://192.168.56.101:8082/monitoring/adapters/zabbix/types/service/groups')
+    rest.get()
+    print rest.body
+    print rest.status_code
+    print rest.headers
+    print "JSON output:"
+    print rest.json_body_load()
+    tenants = rest.json_body_load()
+    print tenants['result']['groups']
