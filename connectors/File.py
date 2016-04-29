4d49517fa48f9b2a1db31733834f507161f06fe4
guarise <andrea.guarise@to.infn.it>
2016-04-29 16:00:26 +0200

    refactoring

--- connectors/File.py
+++ connectors/File.py
@@ -7,22 +7,17 @@ class MetricFile:
 	def __init__(self,name):
 		self.name = name
 		self.fd = open(name)
-		
-	def __next__(self):
-		return self.readline
-		
+			
 	def readline(self):
 		self.lastline = self.fd.readline()
 		return self.lastline
 	
-	def getMetric(self,line):
-		
+	def getMetric(self,line):	
 		(a,b,c,d,e) = line.strip().split(';')
 		h = Hosts.Host(b,a,"")
 		return Metrics.Metric(h,c,d,e)
 		
-	def readMetric(self):
-		
+	def readMetric(self):	
 		(a,b,c,d,e) = self.readline().strip().split(';')
 		h = Hosts.Host(b,a,"")
 		return Metrics.Metric(h,c,d,e)
@@ -43,6 +38,10 @@ class OutputFile:
             output.write(content)
             output.close()
 
-	
+#test code    
+if __name__ == '__main__':
+	file = MetricFile("./test.conf")
+	while file.readline():
+		print "Line:" + file.lastline	
 	
 		
