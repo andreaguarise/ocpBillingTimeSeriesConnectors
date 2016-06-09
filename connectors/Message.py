b34692ed0c01999273e95154e121ea4f0b29e7df
guarise <andrea.guarise@to.infn.it>
2016-06-09 11:38:17 +0200

    improved skeleton

--- connectors/Message.py
+++ connectors/Message.py
@@ -1,13 +1,14 @@
 class Message:
-    def __init__(self,content):
+    def __init__(self,content,destination):
         self.content = content
-    def produce(self): pass
-    def consume(self): pass
+        self.destination = destination
+    def produce(self): pass #subclass must implement
+    def consume(self): pass #subclass must implement
     def __repr__(self):
-        return "Message content: " + self.content
+        return "Message destination: " + self.destination + "\nMessage content: " + self.content
     
 if __name__ == '__main__':
-    msg = Message("Sample\nmessage\ncontent\n")
+    msg = Message("Sample\nmessage\ncontent\n","protocol://destination")
     print msg
     
     
